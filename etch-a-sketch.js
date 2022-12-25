cc3d72dc38927b77ca4d269378d0a241f187e073
Talal-Iqbal <iqbaltalal08@gmail.com>
2022-12-25 21:51:58 +0500

    not as requirment partially working. Many changes

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,38 +1,45 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const gridsize = document.querySelector('#gridsize')
+    const gridSize = document.querySelector('#gridSize')
+    const colour = document.querySelector('#inputColor')
     const containerRows = document.querySelector('#container-rows')
-    const containerColumns = document.querySelector('#container-columns')
-    // const container = document.querySelector('#container')
+
     console.log('why')
-    // function setGrid() {
-    //     // let div;
-    //     // for(i=1;i <=16;i++){
-    //     const div = document.createElement('div')
-    //     // }
-    //     div.style.width = '100px'
-    //     div.style.height = '100px'
-    //     div.style.border = '1px solid black'
-    //     container.appendChild(div)
-    //     console.log('hello')
-    // }
-    gridsize.addEventListener('click', function setGrid(){
-        for(let i=1; i <=16; i++){
+     function setGrid(gridSize){
+        // let gridArea = gridSize
+        function createRows() {
+            for (let i=1; i <=gridSize; i++){
             const divrows = document.createElement('div')
             divrows.classList.add('divrows')
-            // divrows.style.width = '100px'
-            // divrows.style.height = '100px'
-            divrows.style.border = '1px solid black'
+            // divrows.style.border = '1px solid white'
+            for(let i=1; i <=gridSize; i++){
+            const div = document.createElement('div')
+            div.style.width = '100٪'
+            div.style.height = 50 + 'vh' / gridSize
+            div.style.transition = '0.2s 0s ease-in'
+            // div.style.border = '1px solid white'
+            div.style.flex = 'auto'
+            div.classList.add('cell')
+            divrows.appendChild(div)
+            }
             containerRows.appendChild(divrows)
-            console.log('hello')
         }
-        for(let i=1; i <=16; i++){
-            const divcolumns = document.createElement('div')
-            divcolumns.classList.add('divcolumns')
-            // divcolumns.style.width = '100px'
-            // divcolumns.style.height = '100px'
-            divcolumns.style.border = '1px solid black'
-            containerColumns.appendChild(divcolumns)
-            console.log('hello 2')
         }
-    })
+        createRows()
+}
+
+setGrid(16)
+
+
+
+
+    let Cells = document.querySelectorAll('.cell')
+    console.log(Cells)
+    Cells.forEach(cell =>{
+        cell.addEventListener('mouseenter', function color(){
+            cell.style.backgroundColor = colour.value
+        })
+        })
+        // gridtwentyFour.onclick = () => setGrid(24)
+        // gridForty.onclick = () => setGrid(40)
+        // sixtyFour.onclick = () => setGrid(64)
     })
